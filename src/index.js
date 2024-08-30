57287485e51b447198a796941d2dfd31b90d767d
Yazid <yazid780@outlook.fr>
2024-08-31 00:55:12 +0200

    add modal

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const submitButton = document.getElementById("submitButton");
 const email = document.getElementById("email-address");
 const error = document.querySelector(".error");
+const modal = document.querySelector(".modal");
+const dismiss = document.getElementById("btnDismiss");
+const test = document.querySelector(".test");
+
 let regex = new RegExp(/^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/im);
+
 function handleSubmit() {
   console.log(email.value);
 
@@ -9,7 +14,13 @@ function handleSubmit() {
     error.style.display = "block";
   } else {
     error.style.display = "none";
+    modal.style.visibility = "visible";
+    test.style.visibility = "hidden";
   }
 }
-
+function Dismiss() {
+  modal.style.visibility = "hidden";
+  test.style.visibility = "visible";
+}
 submitButton.addEventListener("click", handleSubmit);
+dismiss.addEventListener("click", Dismiss);
